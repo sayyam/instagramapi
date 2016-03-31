81dad6dc1dccbebeb9ce7c6d502a4ed280cf72db
Sayyam <sayyam.mehmood@gmail.com>
2016-03-31 13:24:15 +0500

    Added apidoc

---
+++ instagramapi/build/generated/source/buildConfig/release/com/instagram/instagramapi/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.instagram.instagramapi;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.instagram.instagramapi";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "0.0.4";
+}
